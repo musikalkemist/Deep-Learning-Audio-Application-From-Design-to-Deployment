0923192edc3ec3c397361d101bb699556b977b3f
geopapa11 <gio.fou@gmail.com>
2024-03-07 09:48:20 -0500

    Polishing prepare_dataset.py in Folder 3 a bit.

--- 3- Implementing a Speech Recognition System in TensorFlow 2/prepare_dataset.py
+++ 3- Implementing a Speech Recognition System in TensorFlow 2/prepare_dataset.py
@@ -7,63 +7,60 @@ JSON_PATH = "data.json"
 SAMPLES_TO_CONSIDER = 22050 # 1 sec. of audio
 
 
-def preprocess_dataset(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512):
-    """Extracts MFCCs from music dataset and saves them into a json file.
+def preprocess_dataset(dataset_path: str, json_path: str, num_mfcc: int = 13, n_fft: int = 2048, hop_length: int = 512):
+    r"""Extract MFCCs from audio file.
 
-    :param dataset_path (str): Path to dataset
-    :param json_path (str): Path to json file used to save MFCCs
-    :param num_mfcc (int): Number of coefficients to extract
-    :param n_fft (int): Interval we consider to apply FFT. Measured in # of samples
-    :param hop_length (int): Sliding window for FFT. Measured in # of samples
-    :return:
+    Args:
+        dataset_path: `str`
+            Path to dataset.
+        json_path: `str`
+            Path to json file used to save MFCCs.
+        num_mfcc: `int`, default=13
+            Number of MFCC coefficients.
+        n_fft: `int`, default=2048
+            Length of the FFT window. Measured in # of samples.
+        hop_length: `int`, default=512
+            Sliding window for FFT. Number of samples between successive frames.
+
+    Returns:
     """
-
     # dictionary where we'll store mapping, labels, MFCCs and filenames
     data = {
-        "mapping": [],
-        "labels": [],
-        "MFCCs": [],
-        "files": []
+        'mapping': [],  # mappings of label names to label ids
+        'labels': [],   # y
+        'MFCCs': [],    # x
+        'files': []     # original files for each (x, y) pair
     }
 
-    # loop through all sub-dirs
+    # loop through all the subdirs
     for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
-
-        # ensure we're at sub-folder level
+        filenames = [f for f in filenames if f.endswith('wav')]
+        # ensure we are at subfolder level
         if dirpath is not dataset_path:
+            label_name = dirpath.rsplit('/')[-1]
+            data['mapping'].append(label_name)
+            print(f'\nProcessing: \"{label_name}\"')
 
-            # save label (i.e., sub-folder name) in the mapping
-            label = dirpath.split("/")[-1]
-            data["mapping"].append(label)
-            print("\nProcessing: '{}'".format(label))
-
-            # process all audio files in sub-dir and store MFCCs
+            # loop through all the filenames and extract MFCCs
             for f in filenames:
-                file_path = os.path.join(dirpath, f)
+                filepath = os.path.join(dirpath, f)
 
                 # load audio file and slice it to ensure length consistency among different files
-                signal, sample_rate = librosa.load(file_path)
+                signal, sample_rate = librosa.load(filepath)
 
                 # drop audio files with less than pre-decided number of samples
-                if len(signal) >= SAMPLES_TO_CONSIDER:
-
-                    # ensure consistency of the length of the signal
-                    signal = signal[:SAMPLES_TO_CONSIDER]
-
-                    # extract MFCCs
-                    MFCCs = librosa.feature.mfcc(signal, sample_rate, n_mfcc=num_mfcc, n_fft=n_fft,
-                                                 hop_length=hop_length)
-
-                    # store data for analysed track
-                    data["MFCCs"].append(MFCCs.T.tolist())
-                    data["labels"].append(i-1)
-                    data["files"].append(file_path)
-                    print("{}: {}".format(file_path, i-1))
+                if len(signal) >= NUM_SAMPLES_TO_CONSIDER:
+                    signal = signal[:NUM_SAMPLES_TO_CONSIDER]  # enforce 1 sec long signals
+                    MFCCs = librosa.feature.mfcc(y=signal, sr=sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
+                    data['labels'].append(i-1)
+                    data['MFCCs'].append(MFCCs.T.tolist())
+                    data['files'].append(filepath)
+                    print(f'{filepath}: {i - 1}')
 
     # save data in json file
-    with open(json_path, "w") as fp:
-        json.dump(data, fp, indent=4)
+    with open(json_path, 'w') as f:
+        json.dump(data, f, indent=4)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__:
     preprocess_dataset(DATASET_PATH, JSON_PATH)
